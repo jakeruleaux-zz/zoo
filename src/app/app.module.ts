256045337e95444eed6ebefbcaf6bf12faa65753
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-18 15:02:18 -0700

    add contact about and faqs components for future use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { NewAnimalComponent } from './new-animal/new-animal.component';
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { EditAnimalComponent } from './edit-animal/edit-animal.component';
 import { AnimalAgePipe } from './animalAge.pipe';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -15,11 +19,15 @@ import { AnimalAgePipe } from './animalAge.pipe';
     NewAnimalComponent,
     AnimalListComponent,
     EditAnimalComponent,
-    AnimalAgePipe],
+    AnimalAgePipe,
+    AboutComponent,
+    ContactComponent,
+    FaqsComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,27 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AnimalListComponent } from './animal-list/animal-list.component';
-
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqsComponent } from './faqs/faqs.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: AnimalListComponent
   },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'faqs',
+    component: FaqsComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
