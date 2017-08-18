8a5a7ca9581dfde7912c0ee80ecb68ebbaf34693
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-18 13:07:15 -0700

    add new hard coded animal to objest to test pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent {
   masterAnimalList: Animal[] = [
     new Animal('Tiger', 'Bob', 3, 'People', 'Woods', 9, 'Male', 'Food', 'You', 'Long story', '07/12/2012'),
     new Animal('Whale', 'Moby', 24, 'Sailors', 'Toilet', 1, 'Male', 'Swimming', 'Land', 'Theres a book about this', '08/03/1864'),
-    new Animal('Unicorn', 'Cindy', 4, 'Children', 'Petting area', 4, 'Female', 'Running', 'TV', 'Dont ask', '04/04/2016')
+    new Animal('Unicorn', 'Cindy', 4, 'Children', 'Petting area', 4, 'Female', 'Running', 'TV', 'Dont ask', '04/04/2016'),
+    new Animal('Shark', 'Sam', 1, 'Swimmers', 'Public Pool', 4, 'Male', 'Sleeping', 'Gym', 'I saw a movie about this once', '04/09/2015')
 
   ];
 
